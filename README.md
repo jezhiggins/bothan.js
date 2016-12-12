e67c691bd1c0d4f3d2e04e55cf3c8bef539c2afc
pezholio <pezholio@gmail.com>
2016-12-12 09:34:12 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## bothan.js
+#### A node.js client library for Bothan \- https:\/\/bothan.io
+
+### Installation
+```bash
+npm install bothan.js
+```
+
+### Testing
+```bash
+npm test
+```
---
+++ test/bothan_test.rb
@@ -0,0 +1,47 @@
+require('sepia');
+
+var Bothan = require('../lib/bothan')
+var expect = require('chai').expect
+
+describe('Bothan', function() {
+  describe('#init()', function() {
+
+    it('throws an error if options are missing', function() {
+      expect(function() {
+        Bothan.init({user: 'username', pass: 'password'})
+      }).to.throw(Error, 'The following options are required: endpoint')
+    });
+
+    it('lists all metrics', function(done) {
+      bothan = Bothan.init({user: 'username', pass: 'password', endpoint: 'https://demo.bothan.io'})
+      bothan.methods.listMetrics(function(data, response) {
+        expect(data).to.eql({
+          metrics: [
+            {
+              name: 'metric-with-geodata',
+              url: 'https://demo.bothan.io/metrics/metric-with-geodata.json'
+            },
+            {
+              name: 'metric-with-multiple-values',
+              url: 'https://demo.bothan.io/metrics/metric-with-multiple-values.json'
+            },
+            {
+              name: 'metric-with-target',
+              url: 'https://demo.bothan.io/metrics/metric-with-target.json'
+            },
+            {
+              name: 'metric-with-ytd-target',
+              url: 'https://demo.bothan.io/metrics/metric-with-ytd-target.json'
+            },
+            {
+              name: 'simple-metric',
+              url: 'https://demo.bothan.io/metrics/simple-metric.json'
+            }
+          ]
+        })
+        done()
+      })
+    })
+
+  });
+});
