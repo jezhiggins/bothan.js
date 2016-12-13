02387d404edf78032784a555d5a727d6a6222ffd
pezholio <pezholio@gmail.com>
2016-12-13 11:13:15 +0000

    Use nock-vcr-recorder instead of sepia

--- test/bothan_test.js
+++ test/bothan_test.js
@@ -1,10 +1,10 @@
-require('sepia');
-
 var Bothan = require('../lib/bothan')
 var expect = require('chai').expect
+var vcr = require('nock-vcr-recorder-mocha');
+
 var tk = require('timekeeper');
 
-describe('Bothan', function() {
+vcr.describe('Bothan', function() {
   var bothan;
 
   before(function() {
@@ -23,7 +23,7 @@ describe('Bothan', function() {
     }).to.throw(Error, 'The following options are required: endpoint')
   });
 
-  it('lists all metrics', function(done) {
+  vcr.it('lists all metrics', function(done) {
     bothan.listMetrics(function(data, response) {
       expect(data).to.eql({
         metrics: [
@@ -53,54 +53,54 @@ describe('Bothan', function() {
     })
   })
 
-  it('gets a metric', function(done) {
+  vcr.it('gets a metric', function(done) {
     bothan.getMetric({'metric': 'simple-metric'}, function(data) {
       expect(data).to.eql({
         '_id': {
-          '$oid': '584fccacbe0e7a0004db7322'
+          '$oid': '584fd6680d737a00040e5df2'
         },
         'name': 'simple-metric',
-        'time': '2016-12-13T10:25:48.733+00:00',
-        'value': 17
+        'time': '2016-12-13T11:07:20.401+00:00',
+        'value': 99
       })
       done()
     });
   })
 
-  it('gets a metric for a date', function(done) {
+  vcr.it('gets a metric for a date', function(done) {
     bothan.getMetric({'metric': 'simple-metric', 'dateTime': '2016-12-12T07:00:24.465+00:00'}, function(data) {
       expect(data).to.eql({
         '_id': {
-          '$oid': '584fccacbe0e7a0004db7332'
+          '$oid': '584fd6680d737a00040e5e02'
         },
         'name': 'simple-metric',
-        'time': '2016-12-11T10:25:48.780+00:00',
-        'value': 99
+        'time': '2016-12-11T11:07:20.458+00:00',
+        'value': 13
       })
       done()
     })
   })
 
-  it('gets a metric for a date range', function(done) {
+  vcr.it('gets a metric for a date range', function(done) {
     bothan.getMetric({'metric': 'simple-metric', 'from': '2016-11-12T14:29:37+00:00', 'to': '2016-12-12T14:29:37+00:00'}, function(data) {
       expect(data).to.eql({
         'count': 4,
         'values': [
           {
-            'time': '2016-12-09T10:25:48.820+00:00',
-            'value': 83
+            'time': '2016-12-09T11:07:20.526+00:00',
+            'value': 21
           },
           {
-            'time': '2016-12-10T10:25:48.800+00:00',
-            'value': 22
+            'time': '2016-12-10T11:07:20.500+00:00',
+            'value': 2
           },
           {
-            'time': '2016-12-11T10:25:48.780+00:00',
-            'value': 99
+            'time': '2016-12-11T11:07:20.458+00:00',
+            'value': 13
           },
           {
-            'time': '2016-12-12T10:25:48.760+00:00',
-            'value': 36
+            'time': '2016-12-12T11:07:20.433+00:00',
+            'value': 66
           }
         ]
       })
@@ -108,17 +108,27 @@ describe('Bothan', function() {
     })
   })
 
-  it('creates a simple metric', function(done) {
+  vcr.it('creates a simple metric', function(done) {
     bothan.createMetric({name: 'my-new-metric', value: 123}, function() {
       bothan.getMetric({'metric': 'my-new-metric'}, function(data) {
         expect(data.value).to.eql(123)
-        expect(data.time).to.eql('2016-12-13T10:16:29.613+00:00')
+        expect(data.time).to.eql('2016-01-01T00:00:00.000+00:00')
         done()
       })
     })
   })
 
-  it('creates a metric with a target', function(done) {
+  vcr.it('creates a simple metric with a specific time', function(done) {
+    bothan.createMetric({name: 'my-new-metric', value: 123, time: '2016-12-23T00:00:00.000+00:00'}, function() {
+      bothan.getMetric({'metric': 'my-new-metric'}, function(data) {
+        expect(data.value).to.eql(123)
+        expect(data.time).to.eql('2016-12-23T00:00:00.000+00:00')
+        done()
+      })
+    })
+  })
+
+  vcr.it('creates a metric with a target', function(done) {
     bothan.createTargetMetric({name: 'my-new-target-metric', actual: 123, annual_target: 345, ytd_target: 130}, function() {
       bothan.getMetric({'metric': 'my-new-target-metric'}, function(data) {
         expect(data.value).to.eql({
@@ -131,7 +141,7 @@ describe('Bothan', function() {
     })
   })
 
-  it('creates a metric with geodata', function(done) {
+  vcr.it('creates a metric with geodata', function(done) {
     values =  [
       {
         'type': 'Feature',
